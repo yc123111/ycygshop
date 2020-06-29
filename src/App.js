757f4f0ede09191d9fad767127ac96530aa5f531
yangchuan <190708321@qq.com>
2020-06-29 10:06:58 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import Icon from "./Icon"
+import "lib-flexible"
+
+function App() {
+  return (
+    <div className="App">
+      <Icon></Icon>
+    </div>
+  );
+}
+
+export default App;
